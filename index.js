378d43bf6170dd683d8b742d1d28a4f5a468ab02
centralomd <65143720+centralomd@users.noreply.github.com>
2020-05-27 18:09:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -24,20 +24,14 @@ bot.on('message', message=>{
             message.channel.send('Hi! How are you?')
             break;
         case 'info':
-            if(!args[1]) return message.reply('Available commands: `r!info bot`, `r!info version`.')
-
-            if(args[1] === 'version'){
-                const embed = new Discord.MessageEmbed()
-                .setTitle(version)
-                .setDescription('***This is the cuurent bot version.***')
-                .setColor('0xFFC300')
-                message.channel.send(embed)
-            }
-            if (args[1] === 'bot'){
-                    message.channel.send('This bot currently only send simple texts. More advanced commands coming soon!')
-            }else{
-                message.channel.send('Argument 1 Not found or Invalid Command.')
-            }
+            message.channel.send('This bot currently only send simple texts. More advanced commands coming soon!')
+            break;
+        case 'buildstatus':
+            const embed = new Discord.MessageEmbed()
+            .setTitle(version)
+            .setDescription('***This is the cuurent bot version.***')
+            .setColor('0xFFC300')
+            message.channel.send(embed)
             break;
         case 'clear':
             if(!message.member.roles.cache.find(r => r.name === "Nyam")) return message.channel.send('Action not granted. Reason: "Nyam" role required.')
