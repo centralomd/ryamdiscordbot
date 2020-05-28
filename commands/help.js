c9d83f1828d067b75f0ca1ee00707cc7baff5c61
centralomd <65143720+centralomd@users.noreply.github.com>
2020-05-28 21:16:29 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -13,15 +13,15 @@ module.exports = {
 			.setDescription('Here\'s all the commands on this bot:')
 			.addFields(
 				{ name: '‎', value: '‎'},
-				{ name: 'r!ping', value: '**Test command.**' },
-				{ name: 'r!hello', value: '**Just a simple hello.**' },
-				{ name: 'r!help', value: '**Shows a list of all the commands.**' },
-				{ name: 'r!yeet', value: '**Yeet peoples you mention, just for fun.**' },
-				{ name: 'r!server', value: '**Shows the name of the server this bot is on.**' },
-				{ name: 'r!clear', value: '**Delete/clear messages mentioned.**' },
-				{ name: 'r!avatar', value: '**Shows the avatar of that person.**' },
+				{ name: '**r!ping**', value: '**Test command.**' },
+				{ name: '**r!hello**', value: '**Just a simple hello.**' },
+				{ name: '**r!help**', value: '**Shows a list of all the commands.**' },
+				{ name: '**r!yeet**', value: '**Yeet peoples you mention, just for fun.**' },
+				{ name: '**r!server**', value: '**Shows the name of the server this bot is on.**' },
+				{ name: '**r!clear**', value: '**Delete/clear messages mentioned.**' },
+				{ name: '**r!avatar**', value: '**Shows the avatar of that person.**' },
 			)
-			.setFooter('**Ryam v1b • Help Menu**')
+			.setFooter('Ryam v1b • Help Menu')
 		message.channel.send(helpembed)
 	},
 };
