e92b469f0a00ef35a39c41955a926dd054f6e236
centralomd <65143720+centralomd@users.noreply.github.com>
2020-05-28 17:11:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -46,11 +46,6 @@ const command = args.shift().toLowerCase();
         if (!message.mentions.users.size) {
             return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: "png", dynamic: true })}>`);
         }
-
-        const avatarList = message.mentions.users.map(user => {
-            return `${user.username}'s avatar: <${user.displayAvatarURL({ format: "png", dynamic: true })}>`;
-        });
-        message.channel.send(avatarList);
     }
 
 
