060a30752e78035c0d6befb2a07f026d75a91ca0
centralomd <65143720+centralomd@users.noreply.github.com>
2020-05-29 18:41:00 +0700

    Update gn.js

--- commands/gn.js
+++ commands/gn.js
@@ -5,6 +5,13 @@ module.exports = {
     description: 'Says goodnight to the mentioned user. Respect them.',
     cooldown: 3,
 	execute(message, args) {
+        const gnerror = new Discord.MessageEmbed()
+                .setColor('#E81515')
+                .setTitle('No-one to say goodbye to.')
+                .setDescription('You want to say it to yourself? Mention someone who\'s going to bed!')
+        if (!message.mentions.users.size) {
+            return message.channel.send(gnerror);
+        }
         const taggedUser = message.mentions.users.first();
         const goodnight = new Discord.MessageEmbed()
             .setColor('#7C9EFF')
