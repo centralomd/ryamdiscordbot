79efcb5d781c4065d125637c3595f1cbdd07b70e
centralomd <65143720+centralomd@users.noreply.github.com>
2020-05-31 16:04:49 +0700

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -16,26 +16,25 @@ module.exports = {
             // Join the same voice channel of the author of the message
             if (message.member.voice.channel) {
                 const connection = await message.member.voice.channel.join();
-            }
+            } 
+            const dispatcher = connection.play('http://myserver.com/audio.aac');
+
+            dispatcher.on('start', () => {
+                console.log('Music is now playing!');
+            });
+        
+            // Create a ReadableStream of s16le PCM audio
+            const audio = connection.receiver.createStream(user, { mode: 'pcm' });
+        
+            audio.pipe(fs.createWriteStream('user_audio'));
+        
+            dispatcher.on('finish', () => {
+                console.log('Music has been stopped!');
+            });
+        
+            // Always remember to handle errors appropriately!
+            dispatcher.on('error', console.error);
         });
-
-        // Create a dispatcher
-    const dispatcher = connection.play('http://myserver.com/audio.aac');
-
-    dispatcher.on('start', () => {
-	    console.log('Music is now playing!');
-    });
-
-    // Create a ReadableStream of s16le PCM audio
-    const audio = connection.receiver.createStream(user, { mode: 'pcm' });
-
-    audio.pipe(fs.createWriteStream('user_audio'));
-
-    dispatcher.on('finish', () => {
-	    console.log('Music has been stopped!');
-    });
-
-    // Always remember to handle errors appropriately!
-    dispatcher.on('error', console.error);
+    
     },
 };
