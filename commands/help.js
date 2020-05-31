67d6bd1f12057868c3dc9ada37f74090712d4b98
centralomd <65143720+centralomd@users.noreply.github.com>
2020-05-31 21:31:42 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -13,23 +13,24 @@ module.exports = {
 			.setDescription('Currently available commands:')
 			.addFields(
 				{ name: '‎', value: '‎'},
-				{ name: '***Important Command***', value: '‎'},
+				{ name: '**Basic Commands**', value: '‎'},
 				{ name: '**r!help**', value: 'Shows a list of all the commands.' },
 				{ name: '**r!invite**', value: 'Get the invite link for this bot.'},
-				{ name: '**Basic Commands**', value: '‎'},
-				{ name: '**r!hello**', value: 'Just a simple hello.' },
+				{ name: '**r!hello (shutting down soon)**', value: 'Just a simple hello.' },
+				{ name: '‎', value: '‎'},
 				{ name: '**Fun Commands**', value: '‎'},
 				{ name: '**r!yeet**', value: 'Yeet peoples you mention, just for fun.' },
 				{ name: '**r!gn**', value: 'Says goodnight to the mentioned user. Respect them.'},
 				{ name: '**r!pump**', value: 'Shoot a pump to mentioned user and see how many you damaged them.'},
 				{ name: '**r!snipe**', value: 'Snipe those scared kids and see how much you damaged them.'},
+				{ name: '‎', value: '‎'},
 				{ name: '**Admin Commands**', value: '‎'},
 				{ name: '**r!ping**', value: 'Test command.' },
 				{ name: '**r!server**', value: 'Shows the name of the server this bot is on.' },
 				{ name: '**r!clear**', value: 'Delete/clear messages mentioned.' },
 				{ name: '**r!avatar**', value: 'Shows the avatar of that person.' },
 				{ name: '‎', value: '‎'},
-				{ name: '**Coming Soon!**', value: 'Music Commands, More Fun Comamnds, and more!'},
+				{ name: '**Coming Soon!**', value: 'Apparently we are unable to create a music bot. We will add other features like the fun commands. Stay tuned.'},
 			)
 			.setFooter('Ryam v1b • Help Menu')
 		message.channel.send(helpembed)
