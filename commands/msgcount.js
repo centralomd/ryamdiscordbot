88dac698c8e40205a405ec4eedbee5135090f1a1
centralomd <65143720+centralomd@users.noreply.github.com>
2020-06-01 21:24:05 +0700

    Update msgcount.js

--- commands/msgcount.js
+++ commands/msgcount.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
 const ms = require('ms');
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
 
 module.exports = {
     name: 'msgcount',
