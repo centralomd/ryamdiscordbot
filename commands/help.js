6d7662dffc719d455c30b87a9024084cc92531f6
centralomd <65143720+centralomd@users.noreply.github.com>
2020-06-03 22:05:26 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -9,56 +9,45 @@ module.exports = {
 	usage: '[command name]',
 	cooldown: 10,
 	execute(message, args) {
-		const helpembed = new Discord.MessageEmbed()
+		const helpembedbasic = new Discord.MessageEmbed()
 			.setColor('#E96A00')
-			.setTitle('Command List')
-			.setDescription('Currently available commands:')
+			.setTitle('Commandlist: Basic')
 			.addFields(
 				{ name: '‎', value: '‎'},
-				{ name: '**Basic Commands**', value: '‎'},
 				{ name: '**r!help**', value: 'Shows a list of all the commands.' },
 				{ name: '**r!invite**', value: 'Get the invite link for this bot.'},
 				{ name: '**r!hello (shutting down soon)**', value: 'Just a simple hello.' },
-				{ name: '‎', value: '‎'},
-				{ name: '**Fun Commands**', value: '‎'},
-				{ name: '**r!yeet**', value: 'Yeet peoples you mention, just for fun.' },
-				{ name: '**r!gn**', value: 'Says goodnight to the mentioned user. Respect them.'},
-				{ name: '**r!pump**', value: 'Shoot a pump to mentioned user and see how many you damaged them.'},
-				{ name: '**r!snipe**', value: 'Snipe those scared kids and see how much you damaged them.'},
-				{ name: '**r!fullsweat**', value: 'Full sweat on a mentioned user and see what you did.'},
-				{ name: '‎', value: '‎'},
-				{ name: '**Admin Commands**', value: '‎'},
-				{ name: '**PERMISSION UNVERIFIED!**', value: 'You are only able to view this part if you have "ADMINISTRATOR" permission.' },
-				{ name: '‎', value: '‎'},
-				{ name: '**Coming Soon!**', value: 'Apparently we are unable to create a music bot. We will add other features like the fun commands. Stay tuned.'},
 			)
 			.setFooter('Ryam v1b • Help Menu')
-		const adminhelpembed = new Discord.MessageEmbed()
+
+		const userReactions = message.reactions.cache.filter(reaction => reaction.users.cache.has(userId));
+		
+		
+		const helpembedfun = new Discord.MessageEmbed()
 			.setColor('#E96A00')
-			.setTitle('Command List: Admin')
-			.setDescription('Currently available commands (including Admin commands):')
-			.addFields(
+			.setTitle('Commandlist: Fun')
+			.addField(
 				{ name: '‎', value: '‎'},
-				{ name: '**Basic Commands**', value: '‎'},
-				{ name: '**r!help**', value: 'Shows a list of all the commands.' },
-				{ name: '**r!invite**', value: 'Get the invite link for this bot.'},
-				{ name: '**r!hello (shutting down soon)**', value: 'Just a simple hello.' },
-				{ name: '‎', value: '‎'},
-				{ name: '**Fun Commands**', value: '‎'},
 				{ name: '**r!yeet**', value: 'Yeet peoples you mention, just for fun.' },
 				{ name: '**r!gn**', value: 'Says goodnight to the mentioned user. Respect them.'},
 				{ name: '**r!pump**', value: 'Shoot a pump to mentioned user and see how many you damaged them.'},
 				{ name: '**r!snipe**', value: 'Snipe those scared kids and see how much you damaged them.'},
 				{ name: '**r!fullsweat**', value: 'Full sweat on a mentioned user and see what you did.'},
+			)
+		const helpembedadmin = new Discord.MessageEmbed()
+			.setColor('#E96A00')
+			.setTitle('Commandlist: Admin')
+			.addFields(
 				{ name: '‎', value: '‎'},
-				{ name: '**Admin Commands (You are verified an admin)**', value: '‎'},
 				{ name: '**r!ping**', value: 'Test command.' },
 				{ name: '**r!server**', value: 'Shows the name of the server this bot is on.' },
 				{ name: '**r!clear**', value: 'Delete/clear messages mentioned.' },
 				{ name: '**r!avatar**', value: 'Shows the avatar of that person.' },
-				{ name: '‎', value: '‎'},
-				{ name: '**Coming Soon!**', value: 'Apparently we are unable to create a music bot. We will add other features like the fun commands. Stay tuned.'},
 			)
+		const helpadminfail = new Discord.MessageEmbed()
+			.setColor('#E96A00')
+			.setTitle('Admin Verification: Failure.')
+			.setDescription('You do not have the permissions to access this page. Press the back button to go back to the last page.')
 			.setFooter('Ryam v1b • Help Menu')
 
 		const helpsenddmsuccess = new Discord.MessageEmbed()
@@ -66,57 +55,56 @@ module.exports = {
 			.setTitle(`Verified Admin - Sent in DMs!`)
 			.setDescription(`Help Command List (Admin Included) has been sent to your DMs, ${message.author}!`)
 
-		const helpsenddmsuccessori = new Discord.MessageEmbed()
-			.setColor('#1CE300')
-			.setTitle(`Sent in DMs!`)
-			.setDescription(`Help Command List has been sent to your DMs, ${message.author}!`)
-
 		const helpcancelled = new Discord.MessageEmbed()
-				.setColor('#FC712C')
-				.setTitle('Cancelled.')
-				.setDescription('You either didn\'t respond or pressed ❌.')
-		
-		if (message.member.hasPermission('ADMINISTRATOR')) {
-			message.react('❓').then(() => message.react('❌'));
+			.setColor('#FC712C')
+			.setTitle('Cancelled.')
+			.setDescription('Help command page has been cancelled.')
+			
+		const helpnoresponse = new Discord.MessageEmbed()
+			.setColor('#FC712C')
+			.setTitle('No response.')
+			.setDescription('You didn\'t respond for `30 seconds`.')
 
-		const filter = (reaction, user) => {
-			return ['❓', '❌'].includes(reaction.emoji.name) && user.id === message.author.id;
-	};
+		const noentryembed = new Discord.MessageEmbed()
+			.setColor('#FC712C')
+			.setTitle('Action Blocked.')
+			.setFooter('Deleting this message in 5 seconds.')
 
-		message.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
-			.then(collected => {
-				const reaction = collected.first();
+		const pages = [helpembedbasic, helpembedfun, helpembedadmin];
+		const page = 1;
 
-				if (reaction.emoji.name === '❓') {
-					message.channel.send(helpsenddmsuccess)
-					message.author.send(adminhelpembed);
-				} else {
-					message.channel.send(helpcancelled);
-		}
-	})
-			.catch(collected => {
-				message.channel.send(helpcancelled);
-		});
-			}
-			if (!message.member.hasPermission('ADMINISTRATOR')) { message.react('❓').then(() => message.react('❌'));
-				const filternoadmin = (reaction, user) => {
-				return ['❓', '❌'].includes(reaction.emoji.name) && user.id === message.author.id;
+
+		message.channel.send(helpembedbasic).then(message => {
+			message.react('⬅️').then(() => message.react('➡️'));
+
+			const filter = (reaction, user) => {
+				return ['⬅️', '❌', '➡️'].includes(reaction.emoji.name) && user.id === message.author.id;
 			};
-		
-				message.awaitReactions(filternoadmin, { max: 1, time: 60000, errors: ['time'] })
-					.then(collected => {
-						const reaction = collected.first();
-		
-						if (reaction.emoji.name === '❓') {
-							message.channel.send(helpsenddmsuccessori)
-							message.author.send(helpembed);
-						} else {
-							message.channel.send(helpcancelled);
-				}
-			})
-					.catch(collected => {
-						message.channel.send(helpcancelled);
-				});
-		}
+
+			message.awaitReactions(filter, { max: 1, time: 30000, errors: ['time'] })
+				.then(collected => {
+					const reaction = collected.first();
+
+					if (reaction.emoji.name === '⬅️') {
+						if (page === 1) return;
+						page--;
+						helpembedbasic.setDescription(paegs[page-1]);
+						helpembedbasic.setFooter(`Page ${page} of ${pages.length}`)
+						message.edit(helpembedbasic)
+					} if (reaction.emoji.name === '➡️') {
+						if (page === pages.length) return;
+						page++;
+						helpembedbasic.setDescription(paegs[page-1]);
+						helpembedbasic.setFooter(`Page ${page} of ${pages.length}`)
+						message.edit(helpembedbasic)
+					} if (reaction.emoji.name === '❌') {
+						message.channel.send(helpcancelled)
+					}
+		})
+				.catch(collected => {
+					message.channel.send(helpnoresponse);
+		});
+	})
+//endbrackets
 	},
 };
