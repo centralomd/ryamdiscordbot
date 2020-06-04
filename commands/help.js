9338a6d20549b029ea45e67b64faf1e6cb8cd787
centralomd <65143720+centralomd@users.noreply.github.com>
2020-06-04 19:23:22 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,146 +1,87 @@
-const Discord = require('discord.js');
-const {Client, Attachment, MessageEmbed} = require('discord.js');
-const bot = new Client();
+'use strict';
 
-module.exports = {
-	name: 'help',
-	description: 'List all the available commands.',
-	aliases: ['commands'],
-	usage: '[command name]',
-	cooldown: 10,
-	execute(message, args) {
-		const helpembedbasic = new Discord.MessageEmbed()
-			.setColor('#E96A00')
-			.setTitle('Commandlist: Basic')
-			.addFields(
-				{ name: '‎', value: '‎'},
-				{ name: '**r!help**', value: 'Shows a list of all the commands.' },
-				{ name: '**r!invite**', value: 'Get the invite link for this bot.'},
-				{ name: '**r!hello (shutting down soon)**', value: 'Just a simple hello.' },
-			)
-			.setFooter('Ryam v1b • Help Menu')
+const fs = require('fs');
+const { promisify } = require('util');
 
-		const userReactions = message.reactions.cache.filter(reaction => reaction.users.cache.has(userId));
-		
-		
-		const helpembedfun = new Discord.MessageEmbed()
-			.setColor('#E96A00')
-			.setTitle('Commandlist: Fun')
-			.addField(
-				{ name: '‎', value: '‎'},
-				{ name: '**r!yeet**', value: 'Yeet peoples you mention, just for fun.' },
-				{ name: '**r!gn**', value: 'Says goodnight to the mentioned user. Respect them.'},
-				{ name: '**r!pump**', value: 'Shoot a pump to mentioned user and see how many you damaged them.'},
-				{ name: '**r!snipe**', value: 'Snipe those scared kids and see how much you damaged them.'},
-				{ name: '**r!fullsweat**', value: 'Full sweat on a mentioned user and see what you did.'},
-			)
-		const helpembedadmin = new Discord.MessageEmbed()
-			.setColor('#E96A00')
-			.setTitle('Commandlist: Admin')
-			.addFields(
-				{ name: '‎', value: '‎'},
-				{ name: '**r!ping**', value: 'Test command.' },
-				{ name: '**r!server**', value: 'Shows the name of the server this bot is on.' },
-				{ name: '**r!clear**', value: 'Delete/clear messages mentioned.' },
-				{ name: '**r!avatar**', value: 'Shows the avatar of that person.' },
-			)
-		const helpadminfail = new Discord.MessageEmbed()
-			.setColor('#E96A00')
-			.setTitle('Admin Verification: Failure.')
-			.setDescription('You do not have the permissions to access this page. Press the back button to go back to the last page.')
-			.setFooter('Ryam v1b • Help Menu')
+const getCommands = promisify(fs.readdir);
 
-		const helpsenddmsuccess = new Discord.MessageEmbed()
-			.setColor('#1CE300')
-			.setTitle(`Verified Admin - Sent in DMs!`)
-			.setDescription(`Help Command List (Admin Included) has been sent to your DMs, ${message.author}!`)
+exports.Run = async function Run(caller, command, guild, lang) {
+  const commandList = await getCommands('./commands');
+  const pages = [
+    {
+      embed: {
+        color: caller.color.blue,
+        title: lang.help.categories[0],
+        fields: [],
+        footer: {
+          text: `${lang.help.page} 1/4`,
+        },
+      },
+    },
+    {
+      embed: {
+        color: caller.color.blue,
+        title: lang.help.categories[1],
+        fields: [],
+        footer: {
+          text: `${lang.help.page} 2/4`,
+        },
+      },
+    },
+    {
+      embed: {
+        color: caller.color.blue,
+        title: lang.help.categories[2],
+        fields: [],
+        footer: {
+          text: `${lang.help.page} 3/4`,
+        },
+      },
+    },
+    {
+      embed: {
+        color: caller.color.blue,
+        title: lang.help.categories[3],
+        fields: [],
+        footer: {
+          text: `${lang.help.page} 4/4`,
+        },
+      },
+    },
+  ];
+  commandList.forEach((file) => {
+    try {
+      const cmd = require(`./${file}`); // eslint-disable-line
+      if (cmd.Settings.show) {
+        pages[cmd.Settings.category].embed.fields.push({
+          name:
+            command.prefix +
+            cmd.Settings.command +
+            lang.commands[cmd.Settings.command].params,
+          value: lang.commands[cmd.Settings.command].help,
+        });
+      }
+    } catch (e) {
+      caller.logger.warn(`[Help Command] Error ${e}`);
+    } finally {
+      delete require.cache[require.resolve(`./${file}`)];
+    }
+  });
+  pages.forEach((obj, page) => {
+    pages[page].embed.fields.push({
+      name: lang.help.title,
+      value: lang.help.links.join('\n'),
+    });
+  });
+  let page = command.params[0] ? Number(command.params[0] - 1) : 0;
+  if (page > pages - 1) page = pages.length - 1;
+  if (page < 0) page = 0;
+  caller.utils.pagination(pages, command.msg.channel, command.msg.author.id, page);
+};
 
-		const helpcancelled = new Discord.MessageEmbed()
-			.setColor('#FC712C')
-			.setTitle('Cancelled.')
-			.setDescription('Help command page has been cancelled.')
-			
-		const helpnoresponse = new Discord.MessageEmbed()
-			.setColor('#FC712C')
-			.setTitle('No response.')
-			.setDescription('You didn\'t respond for `30 seconds`.')
-
-		const noentryembed = new Discord.MessageEmbed()
-			.setColor('#FC712C')
-			.setTitle('Action Blocked.')
-			.setFooter('Deleting this message in 5 seconds.')
-
-		message.channel.send(helpembedbasic).then(message => {
-			message.react('⬅️').then(() => message.react('❌')).then(() => message.react('➡️'));
-
-			const filter = (reaction, user) => {
-				return ['⬅️', '❌', '➡️'].includes(reaction.emoji.name) && user.id === message.author.id;
-			};
-
-			message.awaitReactions(filter, { max: 1, time: 30000, errors: ['time'] })
-				.then(collected => {
-					const reaction = collected.first();
-					
-					if (reaction.emoji.name === '➡️') {
-						message.channel.bulkDelete(1, true).then(message =>
-							message.channel.send(helpembedfun).then(message => {
-								message.react('⬅️').then(() => message.react('❌')).then(() => message.react('➡️'));
-					
-								const filter = (reaction, user) => {
-									return ['⬅️', '❌', '➡️'].includes(reaction.emoji.name) && user.id === message.author.id;
-								};
-					
-								message.awaitReactions(filter, { max: 1, time: 30000, errors: ['time'] })
-									.then(collected => {
-										const reaction = collected.first();
-					
-										if (reaction.emoji.name === '⬅️') {
-											message.channel.bulkDelete(1, true)
-										} if (reaction.emoji.name === '➡️') {
-											message.channel.bulkDelete(1, true)
-											if(!message.member.hasPermission("ADMINISTRATOR", explicit = true)) { return message.channel.send(clearadminbad) 
-											} else {
-												message.channel.bulkDelete(1, true).then(message =>
-													message.channel.send(helpembedadmin).then(message => {
-														message.react('⬅️').then(() => message.react('❌')).then(() => message.react('➡️'));
-											
-														const filter = (reaction, user) => {
-															return ['⬅️', '❌'].includes(reaction.emoji.name) && user.id === message.author.id;
-														};
-											
-														message.awaitReactions(filter, { max: 1, time: 30000, errors: ['time'] })
-															.then(collected => {
-																const reaction = collected.first();
-											
-																if (reaction.emoji.name === '⬅️') {
-																	message.channel.bulkDelete(1, true)
-																} if (reaction.emoji.name === '❌') {
-																	message.channel.send(helpcancelled)
-																}
-													})
-															.catch(collected => {
-																message.channel.send(helpnoresponse);
-													});
-												})
-												)
-											}
-										} if (reaction.emoji.name === '❌') {
-											message.channel.send(helpcancelled)
-										}
-							})
-									.catch(collected => {
-										message.channel.send(helpnoresponse);
-							});
-						})
-						)
-					} if (reaction.emoji.name === '❌') {
-						message.channel.send(helpcancelled)
-					}
-		})
-				.catch(collected => {
-					message.channel.send(helpnoresponse);
-		});
-	})
-//endbrackets
-	},
+exports.Settings = {
+  command: 'help',
+  show: false,
+  permissions: [],
+  dm: true,
 };
