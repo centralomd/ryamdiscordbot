5f4fe1b11541fae513a6e617c0e350d5193751a5
centralomd <65143720+centralomd@users.noreply.github.com>
2020-06-04 20:18:15 +0700

    Update testhelp.js

--- commands/testhelp.js
+++ commands/testhelp.js
@@ -1,87 +1,18 @@
-'use strict';
+const Discord = require('discord.js');
 
-const fs = require('fs');
-const { promisify } = require('util');
+module.exports = {
+	name: 'help',
+	description: 'List all the available commands.',
+	aliases: ['commands'],
+	usage: '[command name]',
+	cooldown: 10,
+	execute(message, args) {
 
-const getCommands = promisify(fs.readdir);
+    let messageselect = args.slice(1).join(" ");
 
-exports.Run = async function Run(caller, command, guild, lang) {
-  const commandList = await getCommands('./commands');
-  const pagestest = [
-    {
-      embed: {
-        color: caller.color.blue,
-        title: lang.help.categories[0],
-        fields: [],
-        footer: {
-          text: `${lang.help.page} 1/4`,
-        },
-      },
-    },
-    {
-      embed: {
-        color: caller.color.blue,
-        title: lang.help.categories[1],
-        fields: [],
-        footer: {
-          text: `${lang.help.page} 2/4`,
-        },
-      },
-    },
-    {
-      embed: {
-        color: caller.color.blue,
-        title: lang.help.categories[2],
-        fields: [],
-        footer: {
-          text: `${lang.help.page} 3/4`,
-        },
-      },
-    },
-    {
-      embed: {
-        color: caller.color.blue,
-        title: lang.help.categories[3],
-        fields: [],
-        footer: {
-          text: `${lang.help.page} 4/4`,
-        },
-      },
-    },
-  ];
-  commandList.forEach((file) => {
-    try {
-      const cmd = require(`./${file}`); // eslint-disable-line
-      if (cmd.Settings.show) {
-        pages[cmd.Settings.category].embed.fields.push({
-          name:
-            command.prefix +
-            cmd.Settings.command +
-            lang.commands[cmd.Settings.command].params,
-          value: lang.commands[cmd.Settings.command].help,
-        });
-      }
-    } catch (e) {
-      caller.logger.warn(`[Help Command] Error ${e}`);
-    } finally {
-      delete require.cache[require.resolve(`./${file}`)];
-    }
-  });
-  pages.forEach((obj, page) => {
-    pages[page].embed.fields.push({
-      name: lang.help.title,
-      value: lang.help.links.join('\n'),
-    });
-  });
-  let page = command.params[0] ? Number(command.params[0] - 1) : 0;
-  if (page > pages - 1) page = pages.length - 1;
-  if (page < 0) page = 0;
-  caller.utils.pagination(pages, message.author.id, page);
-};
+  message.channel.fetchMessage().then(message => {
+    message.react(argument[0]) //This is assuming that you are splitting the message content into an array, So it will get the first element.
+  })
 
-exports.Settings = {
-  command: 'help',
-  show: false,
-  permissions: [],
-  dm: true,
+  },
 };
