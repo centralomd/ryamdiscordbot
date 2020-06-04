31e12a7baef1e4a1cfe413951250941f22d0ae70
centralomd <65143720+centralomd@users.noreply.github.com>
2020-06-04 20:54:11 +0700

    Update testhelp.js

--- commands/testhelp.js
+++ commands/testhelp.js
@@ -8,9 +8,14 @@ module.exports = {
 	cooldown: 10,
 	execute(message, args) {
 
+    const failargs = new Discord.MessageEmbed()
+      .setTitle('no args 0 or 1')
+
+    if(!args[0] && !args[1]) return 
+
     let messageselect = args.slice(1).join(" ");
 
-  message.channel.fetchMessage().then(message => {
+  message.channel.fetchMessage(messageselect).then(message => {
     message.react(argument[0]) //This is assuming that you are splitting the message content into an array, So it will get the first element.
   })
 
