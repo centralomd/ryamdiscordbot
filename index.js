6c918cc04028cbcf049478c5749e445407872023
centralomd <65143720+centralomd@users.noreply.github.com>
2020-06-05 23:29:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -24,20 +24,20 @@ client.once('ready', () => {
     client.user.setActivity('r!help', { type: 'LISTENING'}).catch(console.error);
 })
 
-    client.music = new ErelaClient(client, nodes)
-        .on("nodeError", console.log)
-        .on("nodeConnect", () => console.log("Successfully created a new Node."))
-        .on("queueEnd", player => {
-            player.textChannel.send("Queue has ended.")
-            return client.music.players.destroy(player.guild.id)
-        })
-        .on("trackStart", ({textChannel}, {title, duration}) => textChannel.send(`Now playing: **${title}** \`${Utils.formatTime(duration, true)}\``).then(m => m.delete(15000)))
+    //client.music = new ErelaClient(client, nodes)
+    //.on("nodeError", console.log)
+    //.on("nodeConnect", () => console.log("Successfully created a new Node."))
+    //.on("queueEnd", player => {
+    //    player.textChannel.send("Queue has ended.")
+    //    return client.music.players.destroy(player.guild.id)
+    //})
+    //.on("trackStart", ({textChannel}, {title, duration}) => textChannel.send(`Now playing: **${title}** \`${Utils.formatTime(duration, true)}\``).then(m => m.delete(15000)))
 
-        client.levels = new Map()
-        .set("none", 0.0)
-        .set("low", 0.10)
-        .set("medium", 0.15)
-        .set("high", 0.25);
+    //client.levels = new Map()
+    //.set("none", 0.0)
+    //.set("low", 0.10)
+    //.set("medium", 0.15)
+    //.set("high", 0.25);
 
 client.on('message', message=>{
 if (!message.content.startsWith(prefix) || message.author.bot) return;
