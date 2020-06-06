93ba2d6c2f32cb1cd22864b625302f0b0b6c480e
centralomd <65143720+centralomd@users.noreply.github.com>
2020-06-06 10:57:28 +0700

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -5,7 +5,6 @@ module.exports = {
     description: 'Repeats a word the message author said.',
     cooldown: 5,
 	execute(message, args) {
-        message.channel.delete(1)
         if(message.author.bot) return;
         let saymsg = message.content
                 message.channel.send(saymsg.replace("r!say",""))
