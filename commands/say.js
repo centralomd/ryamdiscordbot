0088ee7678b09a7b7cf4257b04276dc30fab9359
centralomd <65143720+centralomd@users.noreply.github.com>
2020-06-06 11:43:19 +0700

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -5,9 +5,11 @@ module.exports = {
     description: 'Repeats a word the message author said.',
     cooldown: 5,
 	execute(message, args) {
-        if(message.author.bot) return;
+        const numberonedelete = 1
+
+        message.author.delete(1)
+        if(message.author.bot) return message.channel.bulkDelete(numberonedelete);
         let saymsg = message.content
                 message.channel.send(saymsg.replace("r!say",""))
-            m => m.delete(10)
     },
 };
