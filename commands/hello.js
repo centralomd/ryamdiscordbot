80d39c7d1d30ab6f6f141c86c02dcf356377d78a
centralomd <65143720+centralomd@users.noreply.github.com>
2020-06-26 18:42:40 +0700

    'reset

--- commands/hello.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'hello',
-    description: 'Just a simple hello.',
-    cooldown: 5,
-    execute(message, args) {
-        message.channel.send('Hi! How are you?');
-    },
-};
